72f1663d6af6b7e603ab2db74ddd8f3a1d1977aa
Simon Larsen <simonhffh@gmail.com>
2018-02-01 15:17:09 +0100

    Converted network files to RDS

--- get_network.R
+++ get_network.R
@@ -1,10 +1,10 @@
 get_network_file <- function(id) {
   path <- switch(id, 
-    biogrid    = paste0("grandforest-web-common/data/biogrid.human.entrez.tsv"),
-    htri       = paste0("grandforest-web-common/data/htri.entrez.tsv"),
-    iidall     = paste0("grandforest-web-common/data/iid.human.all.entrez.tsv"),
-    iidexp     = paste0("grandforest-web-common/data/iid.human.exp.entrez.tsv"),
-    regnetwork = paste0("grandforest-web-common/data/regnetwork.entrez.tsv")
+    biogrid    = paste0("grandforest-web-common/data/biogrid.human.entrez.rds"),
+    htri       = paste0("grandforest-web-common/data/htri.entrez.rds"),
+    iidall     = paste0("grandforest-web-common/data/iid.human.all.entrez.rds"),
+    iidexp     = paste0("grandforest-web-common/data/iid.human.exp.entrez.rds"),
+    regnetwork = paste0("grandforest-web-common/data/regnetwork.entrez.rds")
   )
 }
 
