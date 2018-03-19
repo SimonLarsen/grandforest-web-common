59f3dbee4400c54e78bdaec7e26449b6dc487a36
Simon Larsen <simonhffh@gmail.com>
2018-03-19 14:50:34 +0100

    Fixed zip file reading, removed GZIP support

--- read_data.R
+++ read_data.R
@@ -3,13 +3,18 @@ library(data.table)
 read_expression_file <- function(path) {
   lpath <- tolower(path)
   if(endsWith(lpath, ".zip")) {
-    fread(paste0("unzip -cq ", path), header=TRUE)
-  } else if(endsWith(lpath, ".gz") || endsWith(lpath, ".gzip")) {
-    fread(paste0("zcat ", path), header=TRUE)
+    files <- unzip(path, list=TRUE)
+    fpath <- files$Name[which.max(files$Length)]
+    exdir <- tempfile()
+    ext.files <- unzip(path, files=fpath, overwrite=TRUE, exdir=exdir)
+    read_expression_file(file.path(exdir, fpath))
   } else if(endsWith(lpath, ".rds")) {
     out <- readRDS(path)
     if(!(class(out)[1] %in% c("data.frame","data.table"))) {
       stop("RDS file does not contain a `data.frame` or `data.table` object.")
+    }
+    if(class(out)[1] == "data.frame") {
+        out <- data.table(out)
     }
     return(out)
   } else {
